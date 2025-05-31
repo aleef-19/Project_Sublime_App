f84313474e4928d74c112421a130d4a264e73fcf
Aleef Mohammad Althaf <ee24b086@smail.iitm.ac.in>
2025-05-31 16:36:29 +0530

    added simple linear in R and dataset

---
+++ Linear_reg_11/simple_linear_reg.R
@@ -0,0 +1,26 @@
+# Load data
+df <- read.table("C:/Users/kiros/Downloads/height_weight.txt", header = TRUE)
+
+# Fit linear model
+model <- lm(Weight ~ Height, data = df)
+
+# Output model summary
+summary(model)
+
+# Plot data and regression line
+plot(Weight ~ Height, data = df, main = "Height vs Weight",
+     xlab = "Height", ylab = "Weight", pch = 19, col = "blue")
+abline(model, col = "red", lwd = 2)
+legend("topleft", legend = c("Data", "Regression Line"),
+       col = c("blue", "red"), pch = c(19, NA), lty = c(NA, 1), lwd = c(NA, 2))
+
+
+
+
+
+
+
+
+
+
+
